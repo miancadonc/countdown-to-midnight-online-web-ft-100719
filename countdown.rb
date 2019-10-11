3cce47bd9bf5332e64414c7e9af85c03312cc735
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-11 20:23:02 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(seconds_left)
+  until seconds_left == 0
+    puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1
+    #sleep(1)
+  end
+  new_year_message = "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_left)
+  until seconds_left == 0
+    puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1
+    sleep(1)
+  end
+  p "HAPPY NEW YEAR!"
 end
